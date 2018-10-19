e675c14619ff66d2babd03ca0d53211b41c9b840
CoByte <xwtheiol@gmail.com>
2018-10-19 16:08:40 -0700

    Bugfixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DownClimbAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DownClimbAuto.java
@@ -1,5 +1,28 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class DownClimbAuto {
+
+    DcMotor backLift = null;
+    DcMotor frontLift = null;
+    DcMotor leftDrive = null;
+    DcMotor rightDrive = null;
+
+    DownClimbAuto(DcMotor backLift, DcMotor frontLift, DcMotor leftDrive, DcMotor rightDrive) {
+
+        this.backLift = backLift;
+        this.frontLift = frontLift;
+        this.leftDrive = leftDrive;
+        this.rightDrive = rightDrive;
+    }
+
+    public void run() {
+
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+
+    }
 }
