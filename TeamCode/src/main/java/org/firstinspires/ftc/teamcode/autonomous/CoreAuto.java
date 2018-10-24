f165a323b701f9094bc8a1206adce6ab9a1e19fb
Chase <1chasemcd@gmail.com>
2018-10-24 15:00:57 -0700

    More

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CoreAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/CoreAuto.java
@@ -6,24 +6,12 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.main.Hardware;
 import org.firstinspires.ftc.teamcode.main.PVector;
 
-//733.9539, -733.9539
-//2482.1912
-//
-//695.76056, -695.76056
-//1915.5548
-//
-//-61.434097, 61.434097
-//4454.3467
-
 public abstract class CoreAuto extends LinearOpMode{
 
     private ElapsedTime runtime = new ElapsedTime();
     Hardware robot = new Hardware();
 
     public void encoderLift(double speed, int rotations) {
-
-        robot.leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        robot.rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         int liftTarget = robot.liftArm.getCurrentPosition() + rotations * (int)robot.LIFT_COUNTS_PER_ROT; // 2 needs to be replaced with # of rotations
 
@@ -47,17 +35,17 @@ public abstract class CoreAuto extends LinearOpMode{
 
     public void runPath(PVector[] path) {
         for (PVector move : path) {
-            encoderTurn(0.7, move.t);
-            encoderForward(0.7, move.r);
+            encoderMove(0.7, move.t, -1);
+            encoderMove(0.7, move.r, 1);
         }
     }
 
-    public void encoderForward(double speed, int ticks) {
+    public void encoderMove(double speed, int ticks, int mode) {
 
         // Ensure that the opmode is still active
         if (opModeIsActive()) {
 
-            robot.leftDrive.setTargetPosition(ticks);
+            robot.leftDrive.setTargetPosition(mode * ticks);
             robot.rightDrive.setTargetPosition(ticks);
 
             // Turn On RUN_TO_POSITION
@@ -68,46 +56,6 @@ public abstract class CoreAuto extends LinearOpMode{
             runtime.reset();
             robot.leftDrive.setPower(speed);
             robot.rightDrive.setPower(speed);
-
-            while (opModeIsActive() &&
-                    (runtime.seconds() < 10) &&
-                    (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
-
-                // Display it for the driver.
-                telemetry.addData("Path1",  "Running to %7d :%7d", ticks,  ticks);
-                telemetry.addData("Path2",  "Running at %7d :%7d",
-                        robot.leftDrive.getCurrentPosition(),
-                        robot.rightDrive.getCurrentPosition());
-                telemetry.update();
-            }
-
-            // Stop all motion;
-            robot.leftDrive.setPower(0);
-            robot.rightDrive.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            //  sleep(250);   // optional pause after each move
-        }
-    }
-
-    public void encoderTurn(double speed, int ticks) {
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            robot.leftDrive.setTargetPosition(ticks);
-            robot.rightDrive.setTargetPosition(-ticks);
-
-            // Turn On RUN_TO_POSITION
-            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            robot.leftDrive.setPower(speed);
-            robot.rightDrive.setPower(-speed);
 
             while (opModeIsActive() &&
                     (runtime.seconds() < 10) &&
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/MainAuto.java
@@ -1,12 +1,31 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import org.firstinspires.ftc.teamcode.main.PVector;
+
+//733.9539, -733.9539
+//2482.1912
+//
+//695.76056, -695.76056
+//1915.5548
+//
+//-61.434097, 61.434097
+//4454.3467
+
 public class MainAuto extends CoreAuto {
+
+
+    public PVector[] path = new PVector[3];
 
     public void runOpMode() {
 
         robot.init(hardwareMap);
+        path[0] = new PVector(2482, 734);
+        path[1] = new PVector(1915, 696);
+        path[2] = new PVector(4454, -61);
+
         encoderLift(1.0, 5);
 
+        runPath(path);
 
     }
 }
